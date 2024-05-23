9a8bd084fda4f963acbdee933cb3ea3b17731911
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-23 17:58:21 -0600

    added sale repository

---
+++ HardwareStoreDemo/HardwareStore/Repositories/Sales/ISaleRepository.cs
@@ -0,0 +1,16 @@
+﻿using HardwareStore.Models;
+
+namespace HardwareStore.Repositories.Sales
+{
+    public interface ISaleRepository
+    {
+        Task AddAsync(Sale sale);
+        Task<IEnumerable<Sale>> GetAllAsync();
+        Task<IEnumerable<Client>> GetAllClientsAsync();
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<Sale?> GetSaleByIdAsync(int id);
+        Task<IEnumerable<SaleDetail?>> GetSaleDetailsByIdAsync(int id);
+    }
+}
---
+++ HardwareStoreDemo/HardwareStore/Repositories/Sales/SaleRepository.cs
@@ -0,0 +1,100 @@
+﻿using HardwareStore.Data;
+using HardwareStore.Models;
+
+namespace HardwareStore.Repositories.Sales
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly ISqlDataAccess _dataAccess;
+
+        public SaleRepository(ISqlDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<IEnumerable<Sale>> GetAllAsync()
+        {
+            return await _dataAccess.GetDataAsync<Sale, dynamic>(
+                "dbo.spSale_GetAll",
+                new { }
+                );
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _dataAccess.GetDataAsync<Product, dynamic>(
+                "dbo.spProduct_GetInfoList",
+                new { }
+                );
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            try
+            {
+                var products = await _dataAccess.GetDataAsync<Product, dynamic>(
+                    "dbo.spProduct_GetInfoForSaleById",
+                    new { Id = id }
+                );
+
+                return products.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<SaleDetail?>> GetSaleDetailsByIdAsync(int id)
+        {
+            return await _dataAccess.GetDataAsync<SaleDetail, dynamic>(
+                "dbo.spSalesDetail_GetById",
+                new { Id = id }
+                );
+        }
+
+
+
+        public async Task<IEnumerable<Client>> GetAllClientsAsync()
+        {
+            return await _dataAccess.GetDataAsync<Client, dynamic>(
+                "dbo.spClient_GetInfoList",
+                new { }
+                );
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        {
+            return await _dataAccess.GetDataAsync<Employee, dynamic>(
+                "dbo.spEmployee_GetInfoList",
+                new { }
+                );
+        }
+
+        public async Task<Sale?> GetSaleByIdAsync(int id)
+        {
+            var sale = await _dataAccess.GetDataAsync<Sale, dynamic>(
+                "dbo.spSale_GetById",
+                new { Id = id }
+                );
+
+            return sale.FirstOrDefault();
+        }
+
+        public async Task AddAsync(Sale sale)
+        {
+            // Guarda la venta y recupera el ultimo id generado
+            var saleID = await _dataAccess.SaveDataWithReturnAsync(
+                "dbo.spSale_Insert",
+                new { sale.ClientID, sale.EmployeeID });
+
+            foreach (var saleDetail in sale.SaleDetails) 
+            {
+                saleDetail.SaleID = saleID; // Asignar el SaleID obtenido
+                await _dataAccess.SaveDataAsync("dbo.spSalesDetail_Insert", // inserta los detalles de venta
+                    new { saleDetail.SaleID, saleDetail.ProductID, saleDetail.Quantity });
+            }
+        }
+    }
+}
