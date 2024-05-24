d09c93d8898fb1b040e0ef4e3610412317f00705
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-24 13:11:40 -0600

    added data annotations for employee model

--- HardwareStoreDemo/HardwareStore/Models/Employee.cs
+++ HardwareStoreDemo/HardwareStore/Models/Employee.cs
@@ -1,23 +1,41 @@
-﻿namespace HardwareStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HardwareStore.Models
 {
     public class Employee
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombres")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [Display(Name = "Apellidos")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "El telefono es requerido")]
+        [Display(Name = "Telefono")]
+        [RegularExpression(@"^\d{4}-\d{4}$", ErrorMessage = "El formato debe ser ####-####.")]
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "El email es requerido")]
+        [Display(Name = "Correo Electronico")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "EL salario es requerido")]
+        [Display(Name = "Salario")]
         public decimal Salary { get; set; }
 
+        [Required(ErrorMessage = "La direccion es requerida")]
+        [Display(Name = "Direccion")]
         public string Address { get; set; }
 
+        [Required(ErrorMessage = "La ciudad es requerida")]
+        [Display(Name = "Ciudad")]
         public string City { get; set; }
 
-        public string EmployeeName { get; set; }
+        [Display(Name = "Nombre del Empleado")]
+        public string ?EmployeeName { get; set; }
     }
 }
