b585f5cb7cb5d7e81e3ed3941e03aeaad49ba2f0
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-24 13:08:58 -0600

    added employee repository

---
+++ HardwareStoreDemo/HardwareStore/Repositories/Employees/EmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿using HardwareStore.Data;
+using HardwareStore.Models;
+
+namespace HardwareStore.Repositories.Employees
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ISqlDataAccess _dataAccess;
+
+        public EmployeeRepository(ISqlDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _dataAccess.GetDataAsync<Employee, dynamic>(
+                "dbo.spEmployee_GetAll",
+                new { }
+                );
+        }
+
+        public async Task<Employee?> GetByIdAsync(int id)
+        {
+            var university = await _dataAccess.GetDataAsync<Employee, dynamic>(
+                "dbo.spEmployee_GetById",
+                new { Id = id }
+                );
+
+            return university.FirstOrDefault();
+        }
+
+        public async Task EditAsync(Employee employee)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spEmployee_Update",
+                new {employee.Id, employee.FirstName, employee.LastName, employee.Phone, employee.Email, employee.Salary,
+                    employee.Address, employee.City}
+                );
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spEmployee_Delete",
+                new { Id = id }
+                );
+        }
+
+        public async Task AddAsync(Employee employee)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spEmployee_Insert",
+                new { employee.FirstName, employee.LastName, employee.Phone, employee.Email, employee.Salary, employee.Address, employee.City });
+        }
+    }
+}
---
+++ HardwareStoreDemo/HardwareStore/Repositories/Employees/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using HardwareStore.Models;
+
+namespace HardwareStore.Repositories.Employees
+{
+    public interface IEmployeeRepository
+    {
+        Task AddAsync(Employee employee);
+        Task DeleteAsync(int id);
+        Task EditAsync(Employee employee);
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+    }
+}
