4b097c219dbf6d90b836640825c3f78751776cfa
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-24 13:14:26 -0600

    added dependency injection

--- HardwareStoreDemo/HardwareStore/Program.cs
+++ HardwareStoreDemo/HardwareStore/Program.cs
@@ -1,14 +1,25 @@
+using FluentValidation;
 using HardwareStore.Data;
+using HardwareStore.Models;
+using HardwareStore.Repositories.Employees;
 using HardwareStore.Repositories.Sales;
+using HardwareStore.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// conection database
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 
+// validations
+builder.Services.AddScoped<IValidator<Employee>, EmployeeValidator>();
+
+// repositories
 builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
 
 var app = builder.Build();
 
