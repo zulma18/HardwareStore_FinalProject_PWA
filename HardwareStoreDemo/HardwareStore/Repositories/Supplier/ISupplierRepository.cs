b30dd43f937619ec0c396df861d417603c6024e7
Yulissa_Hrnndz <140362195+Yulissa20220818@users.noreply.github.com>
2024-06-01 16:30:21 -0600

    Add files via upload

---
+++ HardwareStoreDemo/HardwareStore/Repositories/Supplier/ISupplierRepository.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+namespace HardwareStore.Repositories.Supplier
+{
+    public interface ISupplierRepository
+    {
+        Task AddAsync(Models.Supplier supplier);
+        Task AddSupplierAsync(SupplierRepository supplier);
+        Task AddSupplierAsync(Models.Supplier supplier);
+        Task DeleteAsync(int id);
+        Task DeleteSupplierAsync(int id);
+        Task EditAsync(Models.Supplier supplier);
+        Task EditSupplierAsync(Models.Supplier supplier);
+        Task<string?> GetAllAsync();
+        Task<string?> GetAllSuppliersAsync();
+        Task<string?> GetByIdAsync(int id);
+        Task<Supplier> GetByIdSupplierAsync(int id);
+        Task<string?> GetSupplierByIdAsync(int id);
+    }
+}
